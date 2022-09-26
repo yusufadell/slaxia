70261cfcc7e776f32b8546a2221cde5878025164
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-09-26 04:24:22 +0200

    chore: specify what to import when module loads

--- slaxia/__init__.py
+++ slaxia/__init__.py
@@ -3,3 +3,11 @@
 __author__ = """Yusuf Adel"""
 __email__ = "yusufadell.dev@gmail.com"
 __version__ = "0.1.0"
+from slaxia.slaxia import patch_file
+from slaxia.utils import _handle_file_patching, dispatch_slackpath
+
+__all__ = [
+    "patch_file",
+    "_handle_file_patching",
+    "dispatch_slackpath",
+]
